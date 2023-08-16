3154d37be1c5585e143eaf012dc699611b534e3f
lgfas <luis.guilherme2811@gmail.com>
2023-08-16 11:26:39 -0300

    Responsivo Header e Menu Hamburguer

---
+++ Assets/Javascript/menu.js
@@ -0,0 +1,12 @@
+function clickMenu() {
+  if (menu.style.display == "flex") {
+    menu.style.display = "none";
+    nav.style.display = "none";
+    burguer.src = "./Assets/Img/Icons/menu-hamburguer.svg";
+  } else {
+    menu.style.display = "flex";
+    menu.style.flexDirection = "column";
+    nav.style.display = "block";
+    burguer.src = "./Assets/Img/Icons/menu-close.svg";
+  }
+}
