b5bdf06f6ebc77f9aad6b2955d1c9af6be816253
matheus <matheus.fdac@gmail.com>
2023-08-16 22:04:39 -0300

    finalizada responsividade, rodape e formulario

--- Assets/Javascript/menu.js
+++ Assets/Javascript/menu.js
@@ -11,3 +11,19 @@ function clickMenu() {
   }
 }
 
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+  const content = item.querySelector('.accordion-content');
+  const icon = item.querySelector('.accordion-icon');
+
+  header.addEventListener('click', () => {
+    item.classList.toggle('open');
+    if (item.classList.contains('open')) {
+      icon.innerHTML = '-';
+    } else {
+      icon.innerHTML = '+';
+    }
+  });
+});
